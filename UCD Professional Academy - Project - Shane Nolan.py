a6aa83af06e4b187b2541e5ea928efe1b7d4b134
Shane <spnolan10@gmail.com>
2021-03-11 14:52:28 +0000

    Project Requirements

--- UCD Professional Academy - Project - Shane Nolan.py
+++ UCD Professional Academy - Project - Shane Nolan.py
@@ -1,3 +1,27 @@
 #UCD Professional Academy - Project Submission - Shane Nolan
 
-# Testing for Backup on Github 2
+# 1) Real World Scenario
+    # Project should use a real world dataset and include a reference of their source in the report [1]
+
+# 2) Importing data
+    # Your project should make use of one or more of the following, [1]
+    # Relational Database or API or Web Scraping
+    # Import a CSV file into a Pandas DataFrame.
+
+# 3) Analyzing data
+    # Your project should include sorting, indexing, grouping. [1]
+    # Replace missing values or dropping duplicates. [1]
+    # Slicing, loc or iloc. [1]
+    # Looping, iterrows [1]
+    # Merge dataframes [1]
+
+# 4) Python
+    # Use functions to create reusable code. [1]
+    # Numpy. [1]
+    # Dictionary or Lists. [1]
+
+# 5) Visualize
+    # Seaborn, Matplotlib [2]
+
+# 6) Generate Valuable Insights
+    # 5 insights from the visualization. [2]
